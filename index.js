d6a2f8ec596426259046518a9dc80710f0c59864
Sidsha242 <sdemail2244@gmail.com>
2023-03-03 19:47:37 +0530

    3-3-23

--- index.js
+++ index.js
@@ -44,8 +44,6 @@ app.post("/webhook", (req, res) => {
 
     var phone_number_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
     var from = body_param.entry[0].changes[0].value.messages[0].from;
-    var msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
-
 
     if (body_param.object) {
 
@@ -59,6 +57,7 @@ app.post("/webhook", (req, res) => {
             body_param.entry[0].changes[0].value.messages[0].type == 'text'
 
         ) {
+            var msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
             console.log(from);
             console.log(msg_body);
