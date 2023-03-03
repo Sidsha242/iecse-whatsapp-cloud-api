271e9adb1c376c4f8bd43473daa75873edba36e3
Sidsha242 <sdemail2244@gmail.com>
2023-03-03 15:36:07 +0530

    3-3-23

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ app.post("/webhook", (req, res) => {
 
     console.log(JSON.stringify(body_param, null, 2));
 
+
     if (body_param.object) {
         if (
 
@@ -56,6 +57,10 @@ app.post("/webhook", (req, res) => {
             let phone_number_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
             let from = body_param.entry[0].changes[0].value.messages[0].from;
             let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+            console.log(from);
+            console.log(msg_body);
+
 
             axios({
                 method: "POST",
